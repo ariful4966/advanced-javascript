b98fe705a5a0ec90553467c43c3c15373ed39d37
unknown <ariful4966@gmail.com>
2020-03-04 21:14:15 +0600

    false value

---
+++ true-false.js
@@ -0,0 +1,21 @@
+/*//False value
+1. 0 Zero
+2. "" empty String
+3. undefined
+4. null
+5. NaN
+6. false
+
+// Truth value
+all other value is true
+*/
+
+
+let name = " ";
+
+if(name){
+    console.log("the condition is true");
+
+}else{
+    console.log("the condition is false")
+} 
