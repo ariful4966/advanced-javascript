d5ba22d5e2e281f74057d2ef3bb517999eec0538
unknown <ariful4966@gmail.com>
2020-03-05 06:28:45 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+//1. let pakhi;
+/*02. function add(num1, num2){
+        console.log(num1 + num2);
+    }*/
+/*03.   function add(num1, num2){
+        console.log(num1 + num2);
+        return
+    }*/
+ /*04.   const premik = {name:"Ashik khan", phone:017934};
+    console.log(premik.gf);*/
+
+/*05. (const fun = undefined;
+    console.log(fun);*/
+// 06. null
